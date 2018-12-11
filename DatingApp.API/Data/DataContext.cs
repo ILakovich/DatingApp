c0467f71ee2250a936eb39ba87715fcc311f4029
ILakovich <ilakovic@hotmail.com>
2018-12-11 21:49:31 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+
+using DatingApp.API.wwwroot.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.wwwroot.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+            
+        
+    }
+}
